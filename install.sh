4c56e9e29bf506591587e3a72b2a26efe0e18907
Sean Seefried <sean.seefried@gmail.com>
2009-11-25 15:25:34 +1100

    Now ask for confirmation.

--- install.sh
+++ install.sh
@@ -4,6 +4,26 @@
 
 which cpp 2>&1 > /dev/null || (echo "cpp not found in path"; exit 1) 2>&1 
 
+if [ -f ~/.gitconfig ]; then
+  echo "********************************************************"
+  echo "*** This will overwrite anything that already exists ***"
+  echo "********************************************************"
+
+  echo -n "Are you sure you wish to proceed? (y/N)? "
+  read CONFIRM
+  CONFIRM=$(echo $CONFIRM | tr '[:upper:]' '[:lower:]')
+
+  case $CONFIRM in
+      y|yes)
+	  ;;
+      *)
+	  echo "Exiting"
+	  exit 1
+	  ;;
+  esac
+fi
+  
+
 echo "What default email do you want on this machine?"
 read EMAIL
 
